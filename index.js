ff4a5aff2d8160cd0d94fd62eedde44e03310b4d
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-04-30 14:25:51 +0300

    feat: add title

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
+function createTitle() {
+  const title = document.createElement('h1');
+  title.className = 'title';
+  title.innerHTML=('Virtual keyboard by Kate Goncharick')
+  document.appendChild(title);
+}
+
+const keyboard = [
+  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
+  ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', ' /n ', 'del'],
+  ['caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '&#59;', '&#39;', 'enter'],
+  ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '&#44;', '.', '/', '⯅', 'shift'],
+  ['ctrl', 'win', 'alt', ' ', 'alt', 'ctrl', '⯇', '⯆', '⯈'],
+
+  // "sixthRow": ["f1","f2","f3","f4","f5","f6","f7","f8","f9","f10","f11","f12",],
+];
+function addElement() {
+  const textarea = document.createElement('textarea');
+  textarea.setAttribute('autofocus', 'autofocus');
+  document.onclick = () => {
+    textarea.focus();
+  };
+  document.body.insertAdjacentElement('beforeBegin', textarea);
+
+  keyboard.forEach((keysArr) => {
+    const row = document.createElement('div');
+    row.className = 'row';
+    document.body.appendChild(row);
+
+    keysArr.forEach((key) => {
+      const buttonDiv = document.createElement('div');// create div
+      buttonDiv.className = 'key'; // make it look like a button
+      buttonDiv.innerHTML = ([key]); // put in into the row --- create row for each line
+
+      row.insertAdjacentElement('beforeEnd', buttonDiv);
+    });
+
+    // debugger
+  });
+
+}
 
 document.body.onload = addElement;
-// function activeButton (element) {
-//    element.className="active";
-// }
-// buttonDiv.addEventListener("click", activeButton);
-
-let keyboard = [
-   ["`","1","2","3","4","5","6","7","8","9","0","-","=","backspace"],
-   ["tab","q","w","e","r","t","y","u","i","o","p","[","]"," \ ", "del"],
-   ["caps","a","s","d","f","g","h","j","k","l","&#59;","&#39;","enter"],
-   ["shift","z","x","c","v","b","n","m","&#44;",".","/","⯅","shift"],
-   ["ctrl", "win","alt"," ","alt","ctrl","⯇","⯆","⯈"],
-   
-   // "sixthRow": ["f1","f2","f3","f4","f5","f6","f7","f8","f9","f10","f11","f12",],
-]
-function addElement() {
-   const textarea = document.createElement('textarea');
-   textarea.setAttribute('autofocus', 'autofocus');
-   document.onclick = () => {
-     textarea.focus();
-   };
-   document.body.insertAdjacentElement("beforeBegin", textarea);
-   for (let keysArr of keyboard) {
-      const row = document.createElement("div");
-      row.className="row";
-      document.body.appendChild(row);
-
-   for (let key of keysArr) { 
-         const buttonDiv = document.createElement("div");//create div
-         buttonDiv.className="key"; //make it look like a button
-         buttonDiv.innerHTML=([key]); //put in into the row --- create row for each line
-         
-         row.insertAdjacentElement("beforeEnd" , buttonDiv);
-         //debugger
-     }
-   }
-}
 
 // function createButton (size, )
 
+// win - prevent default
+// по клику на кнопку - класс active - визуально меняется
 
+// сначала нагенерить каждую строку отдельно из разных кнопок,
+//  потом сложить получившиеся строки в клавиатуру
 
-//win - prevent default
-//по клику на кнопку - класс active - визуально меняется
+// функция, которая принимает - размер, поведение,
+//  содержание (выполнить для каждой кнопки - объект со свойствами?)
 
-//сначала нагенерить каждую строку отдельно из разных кнопок, потом сложить получившиеся строки в клавиатуру
-
-//функция, которая принимает - размер, поведение, содержание (выполнить для каждой кнопки - объект со свойствами?)
-
-
-//textarea.value = textarea.value.slice(0, textarea.value.length - 1) 
+// textarea.value = textarea.value.slice(0, textarea.value.length - 1)
 // для backspace
 
-//document.addEventListener('keydown', (event) => {
+// document.addEventListener('keydown', (event) => {
 //    <textarea>.innerText = event.key;
 // })
 
-//key code
-//отдельные классы для кнопок?
+// key code
+// отдельные классы для кнопок?
